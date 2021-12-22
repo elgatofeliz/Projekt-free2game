47c5e90aa759be0d59f914fa8cb356480cddcc20
MaxBePunkt <maximillianborret@gmail.com>
2021-12-22 14:55:37 +0100

    tag3 14:32 apiProbs1.6

--- src/pages/All.js
+++ src/pages/All.js
@@ -23,10 +23,14 @@ class All extends Component {
         "x-rapidapi-key": Api,
       },
     })
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ Data: data }, () => { console.log(this.state.Data) })
-        this.setState({ workData: data }, () => { console.log(this.state.workData) })
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ Data: data }, () => {
+          console.log(this.state.Data);
+        });
+        this.setState({ workData: data }, () => {
+          console.log(this.state.workData);
+        });
       })
       .catch((err) => console.log(err));
   }
@@ -38,117 +42,135 @@ class All extends Component {
   searchPlatform = (input) => {
     if (this.state.platformFilterActive === false) {
       if (input === "PC (Windows)" || input === "Web Browser") {
-        console.log(input + "works")
+        console.log(input + "works");
         let searchArrayPlatform = this.state.workData.filter((elt) => {
-          return elt.platform == input
-        })
+          return elt.platform == input;
+        });
 
-        this.setState({ workData: searchArrayPlatform })
-        this.setState({ platformFilter: input })
-        this.setState({ platformFilterActive: true })
+        this.setState({ workData: searchArrayPlatform });
+        this.setState({ platformFilter: input });
+        this.setState({ platformFilterActive: true });
 
-        console.log(this.state.platformFilter)
-        console.log(this.state.platformFilterActive)
-
+        console.log(this.state.platformFilter);
+        console.log(this.state.platformFilterActive);
       }
-    }
-    else if (this.state.platformFilterActive == true && this.state.genreFilterActive == true) {
+    } else if (
+      this.state.platformFilterActive === true &&
+      this.state.genreFilterActive === true
+    ) {
       let searchArrayGenre = this.state.Data.filter((elt) => {
-        return elt.genre == this.state.genreFilter
-      })
+        return elt.genre === this.state.genreFilter;
+      });
       let searchArrayPlatform = searchArrayGenre.filter((elt) => {
-        return elt.platform == input
-      })
+        return elt.platform === input;
+      });
 
-      this.setState({ workData: searchArrayPlatform })
-      this.setState({ platformFilter: input })
-
-    } else if (this.state.platformFilterActive == true && this.state.genreFilterActive == false) {
+      this.setState({ workData: searchArrayPlatform });
+      this.setState({ platformFilter: input });
+    } else if (
+      this.state.platformFilterActive === true &&
+      this.state.genreFilterActive === false
+    ) {
       let searchArrayPlatform = this.state.Data.filter((elt) => {
-        return elt.platform == input
-      })
+        return elt.platform === input;
+      });
 
-      this.setState({ workData: searchArrayPlatform })
-      this.setState({ platformFilter: input })
-      console.log("Works")
-      console.log(this.state.Data)
+      this.setState({ workData: searchArrayPlatform });
+      this.setState({ platformFilter: input });
+      console.log("Works");
+      console.log(this.state.Data);
     }
-  }
+  };
 
   removePlatform = () => {
-    console.log(this.state.platformFilterActive)
-    if (this.state.genreFilterActive == true && this.state.platformFilterActive == true) {
+    console.log(this.state.platformFilterActive);
+    if (
+      this.state.genreFilterActive === true &&
+      this.state.platformFilterActive === true
+    ) {
       let searchArrayPlatform = this.state.Data.filter((elt) => {
-        return elt.genre == this.state.genreFilter
-      })
+        return elt.genre === this.state.genreFilter;
+      });
 
-      this.setState({ workData: searchArrayPlatform })
-      this.setState({ platformFilterActive: false })
-      this.setState({ platformFilter: "" })
-
-    } else if (this.state.platformFilterActive == true && this.state.genreFilterActive == false) {
-      this.setState({ workData: this.state.Data })
-      this.setState({ platformFilterActive: false })
-      this.setState({ platformFilter: "" })
+      this.setState({ workData: searchArrayPlatform });
+      this.setState({ platformFilterActive: false });
+      this.setState({ platformFilter: "" });
+    } else if (
+      this.state.platformFilterActive === true &&
+      this.state.genreFilterActive === false
+    ) {
+      this.setState({ workData: this.state.Data });
+      this.setState({ platformFilterActive: false });
+      this.setState({ platformFilter: "" });
     }
-  }
+  };
 
   // #####################################################################
   // ####################         Genre Suche        #####################
   // #####################################################################
 
   searchGenre = (input) => {
-    if (this.state.genreFilterActive == false) {
+    if (this.state.genreFilterActive === false) {
       let searchArrayGenre = this.state.workData.filter((elt) => {
-        return elt.genre == input
-      })
+        return elt.genre === input;
+      });
 
-      this.setState({ workData: searchArrayGenre })
-      this.setState({ genreFilter: input })
-      this.setState({ genreFilterActive: true })
+      this.setState({ workData: searchArrayGenre });
+      this.setState({ genreFilter: input });
+      this.setState({ genreFilterActive: true });
 
-      console.log(this.state.genreFilter)
-      console.log(this.state.genreFilterActive)
-
-    } else if (this.state.genreFilterActive == true && this.state.platformFilterActive == true) {
-      let searchArrayPlatform = this.state.Data.filter((elt => {
-        return elt.platform == this.state.platformFilter
-      }))
+      console.log(this.state.genreFilter);
+      console.log(this.state.genreFilterActive);
+    } else if (
+      this.state.genreFilterActive === true &&
+      this.state.platformFilterActive === true
+    ) {
+      let searchArrayPlatform = this.state.Data.filter((elt) => {
+        return elt.platform === this.state.platformFilter;
+      });
       let searchArrayGenre = searchArrayPlatform.filter((elt) => {
-        return elt.genre == input
-      })
+        return elt.genre === input;
+      });
 
-      this.setState({ workData: searchArrayGenre })
-      this.setState({ genreFilter: input })
-
-    } else if (this.state.genreFilterActive == true && this.state.platformFilterActive == false) {
+      this.setState({ workData: searchArrayGenre });
+      this.setState({ genreFilter: input });
+    } else if (
+      this.state.genreFilterActive === true &&
+      this.state.platformFilterActive === false
+    ) {
       let searchArrayGenre = this.state.Data.filter((elt) => {
-        return elt.genre == input
-      })
+        return elt.genre === input;
+      });
 
-      this.setState({ workData: searchArrayGenre })
-      this.setState({ genreFilter: input })
+      this.setState({ workData: searchArrayGenre });
+      this.setState({ genreFilter: input });
     }
-  }
+  };
 
   removeGenre = () => {
-    if (this.state.genreFilterActive == true && this.state.platformFilterActive == true) {
+    if (
+      this.state.genreFilterActive === true &&
+      this.state.platformFilterActive === true
+    ) {
       let searchArrayGenre = this.state.Data.filter((elt) => {
-        return elt.platform == this.state.platformFilter
-      })
+        return elt.platform === this.state.platformFilter;
+      });
 
-      this.setState({ workData: searchArrayGenre })
-      this.setState({ genreFilterActive: false })
-      this.setState({ genreFilter: "" })
-      console.log(searchArrayGenre)
-    } else if (this.state.genreFilterActive == true && this.state.platformFilterActive == false) {
-      this.setState({ workData: this.state.Data })
-      this.setState({ genreFilterActive: false })
-      this.setState({ genreFilter: "" })
-      console.log("Works")
-      console.log(this.state.Data)
+      this.setState({ workData: searchArrayGenre });
+      this.setState({ genreFilterActive: false });
+      this.setState({ genreFilter: "" });
+      console.log(searchArrayGenre);
+    } else if (
+      this.state.genreFilterActive === true &&
+      this.state.platformFilterActive === false
+    ) {
+      this.setState({ workData: this.state.Data });
+      this.setState({ genreFilterActive: false });
+      this.setState({ genreFilter: "" });
+      console.log("Works");
+      console.log(this.state.Data);
     }
-  }
+  };
 
   render() {
     return (
@@ -158,7 +180,9 @@ class All extends Component {
         </header>
         <div>
           <button onClick={() => this.searchGenre("MMO")}>MMO</button>
-          <button onClick={() => this.searchGenre("Card Game")}>Card Game</button>
+          <button onClick={() => this.searchGenre("Card Game")}>
+            Card Game
+          </button>
           <button onClick={() => this.searchGenre("MMORPG")}>MMORPG</button>
           <button onClick={() => this.searchGenre("Shooter")}>Shooter</button>
           <button onClick={() => this.searchGenre("Strategy")}>Strategy</button>
@@ -168,14 +192,28 @@ class All extends Component {
           <button onClick={() => this.searchGenre("Social")}>Social</button>
           <button onClick={() => this.searchGenre("Fighting")}>Fighting</button>
 
-          <button onClick={() => this.searchPlatform("Web Browser")}>Web Browser</button>
-          <button onClick={() => this.searchPlatform("PC (Windows)")}>PC (Windows)</button>
-          <h2 style={{
-            display: this.state.platformFilterActive ? "block" : "none"
-          }}><button onClick={() => this.removePlatform()}>X</button>{this.state.platformFilter}</h2>
-          <h2 style={{
-            display: this.state.genreFilterActive ? "block" : "none"
-          }}><button onClick={() => this.removeGenre()}>X</button>{this.state.genreFilter}</h2>
+          <button onClick={() => this.searchPlatform("Web Browser")}>
+            Web Browser
+          </button>
+          <button onClick={() => this.searchPlatform("PC (Windows)")}>
+            PC (Windows)
+          </button>
+          <h2
+            style={{
+              display: this.state.platformFilterActive ? "block" : "none",
+            }}
+          >
+            <button onClick={() => this.removePlatform()}>X</button>
+            {this.state.platformFilter}
+          </h2>
+          <h2
+            style={{
+              display: this.state.genreFilterActive ? "block" : "none",
+            }}
+          >
+            <button onClick={() => this.removeGenre()}>X</button>
+            {this.state.genreFilter}
+          </h2>
         </div>
         <section className="items-wrap">
           {this.state.workData.map((elt) => (
@@ -191,7 +229,7 @@ class All extends Component {
             />
           ))}
         </section>
-      </section >
+      </section>
     );
   }
 }
